46a0d6afe6f3b50ab2d1a0dd5f0f08b731b5993d
pronist <pronist@naver.com>
2021-01-23 16:23:22 +0900

    https://pronist.tistory.com/108

--- programmers/_42840/42840.go
+++ programmers/_42840/42840.go
@@ -7,15 +7,17 @@ import (
 )
 
 type P struct {
-	pattern []int
-	i       int
-	Count   int
+	pattern []int // 각 수포자의 패턴
+	i       int // 내부 이터레이터
+	Count   int // 문제를 맞춘 횟수
 }
 
 func (p *P) Resolve(answer int) {
+	// 내부 이터레이터가 패턴의 마지막에 도달한 경우 리셋해야 한다.
 	if p.i >= len(p.pattern) {
 		p.i = 0
 	}
+	// 패턴과 문제의 답이 일치하는 경우 Count 를 증가시킨다.
 	if answer == p.pattern[p.i] {
 		p.Count++
 	}
@@ -26,11 +28,13 @@ func biggest(counts []int) []int {
 	max := math.MinInt8
 	biggest := make([]int, 0)
 
+	// 가장 큰 값을 찾는다.
 	for _, count := range counts {
 		if max < count {
 			max = count
 		}
 	}
+	// 가장 큰 값이 몇개가 들어있는지 찾는다.
 	for i, count := range counts {
 		if max == count {
 			biggest = append(biggest, i+1)
@@ -41,15 +45,21 @@ func biggest(counts []int) []int {
 
 func solution(answers []int) []int {
 	persons := []*P{
-		{[]int{1, 2, 3, 4, 5}, 0, 0},
-		{[]int{2, 1, 2, 3, 2, 4, 2, 5}, 0, 0},
-		{[]int{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}, 0, 0},
+		{[]int{1,2,3,4,5}, 0, 0},
+		{[]int{2,1,2,3,2,4,2,5}, 0, 0},
+		{[]int{3,3,1,1,2,2,4,4,5,5}, 0, 0},
 	}
+	counts := make([]int, 0)
+
 	for _, answer := range answers {
 		for _, p := range persons {
 			p.Resolve(answer)
 		}
 	}
 
-	return biggest([]int{persons[0].Count, persons[1].Count, persons[2].Count})
+	for _, p := range persons {
+		counts = append(counts, p.Count)
+	}
+
+	return biggest(counts)
 }
