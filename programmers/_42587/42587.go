02ccb0cb4e5bea96e15202ab2896e827898c87f3
pronist <pronist@naver.com>
2021-03-05 21:58:09 +0900

    https://pronist.tistory.com/119

--- programmers/_42587/42587.go
+++ programmers/_42587/42587.go
@@ -14,21 +14,27 @@ Loop:
 	for {
 		for _, priority := range queue {
 			if priority > queue[0] {
+				// 큐의 맨 끝으로 보낸다.
 				queue = append(queue, queue[0])
 				queue = queue[1:]
 
+				// 규칙에 의한 target 처리
 				switch target {
 				case 0:
 					target = len(queue) - 1
 				default:
 					target--
 				}
+
+				// 이미 처리를 했다면 더 이상 진행할 필요가 없으므로 아래의 코드는 생략.
 				continue Loop
 			}
 		}
+		// 여기서 target 이 0 이 된다는 것은 큐에서 target 을 제외하고 모두 빠져나갔다는 의미다.
 		if target == 0 {
 			break Loop
 		}
+		// 큐에서 원소를 하나 제거하고 target, answer 처리.
 		queue = queue[1:]
 		target, answer = target-1, answer+1
 	}
